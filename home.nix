12f03e066343b759eba0c74b6111e85f647ad4d1
MultiTaskingNoob <multitaskingnub@gmail.com>
2025-08-25 23:54:40 +0300

    Added .bashrc and starship.toml

--- home.nix
+++ home.nix
@@ -34,5 +34,7 @@ in
     ".config/i3/config".source = ./dotfiles/i3/config;
     ".config/nvim".source = ./dotfiles/nvim;
     ".config/picom/picom.conf".source = ./dotfiles/picom/picom.conf;
+    "/home/multitasker/.bashrc".source = ./dotfiles/bash/.bashrc;
+    ".config/starship.toml".source = ./dotfiles/starship/starship.toml;
   };
 }
