41191daf8b7016a00e2bc2bf626259b85910730d
MultiTaskingNoob <multitaskingnub@gmail.com>
2025-09-11 18:45:49 +0300

    Updated Neovim Bindings

--- dotfiles/nvim/lua/config/keymap.lua
+++ dotfiles/nvim/lua/config/keymap.lua
@@ -1,3 +1,4 @@
+vim.g.mapleader = " "
 vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", {})
 vim.keymap.set("n", "+", "<CMD>Neotree<CR>", {})
 vim.keymap.set("n", "<C-t>", "<CMD>tabnew<CR>", {})
@@ -5,4 +6,6 @@ vim.keymap.set("n", "<C-q>", "<CMD>q<CR>", {})
 vim.keymap.set("n", "<C-s>", "<CMD>w<CR>", {})
 vim.keymap.set("n","<leader>a","<CMD>FloatermNew<CR>", {})
 vim.keymap.set("n","<C-e>","<CMD>FloatermNext<CR>")
+vim.keymap.set("n", "<leader>f", "<CMD>FzfLua files<CR>", {})
+vim.keymap.set("n", "-", "<CMD>FzfLua<CR>", {})
 
